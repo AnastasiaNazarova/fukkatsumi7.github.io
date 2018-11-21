4c498bfc02f8f38abb1177c116600f12d0c3d45c
fukkatsumi7 <44060503+fukkatsumi7@users.noreply.github.com>
2018-11-21 20:55:31 +0400

    more scripts

--- scripts/script.js
+++ scripts/script.js
@@ -76,3 +76,9 @@ function goUp() {
    } else clearTimeout(timeOut);
 }
 
+function openImageWindow(src) {
+    var image = new Image();
+    image.src = src;
+    var width = image.width;
+    var height = image.height;
+    window.open(src,"Image","width=" + width + ",height=" + height);}
